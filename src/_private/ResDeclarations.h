8e18a9c17120dad00690fd5b48c9cac8986e56a4
Vladimir Rogozin <77537827+vladimirrogozin@users.noreply.github.com>
2021-02-17 17:57:57 +0300

    Update ResDeclarations.h

--- src/_private/ResDeclarations.h
+++ src/_private/ResDeclarations.h
@@ -1,3 +1,12 @@
+/**
+ * @file    ResDeclarations.h
+ * @brief   Shared declarations for Res.
+ *
+ * Copyright (c) 2020-2021 Vladimir Rogozin (vladimir20040609@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
 #ifndef RED_RESDECLARATIONS_H
 #define RED_RESDECLARATIONS_H
 
